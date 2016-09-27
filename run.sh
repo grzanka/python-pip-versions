a1448bebb8c8d3a6467161ae019514e45fa62485
Leszek Grzanka <grzanka@agh.edu.pl>
2016-09-27 19:31:44 +0200

    more images

--- run.sh
+++ run.sh
@@ -2,9 +2,21 @@
 
 images=(
     "debian:testing"
+    "debian:stable"
+    "debian:7"
+    "debian:8"
     "ubuntu:12.04"
     "ubuntu:14.04"
     "ubuntu:16.04"
+    "ubuntu:16.10"
+    "fedora:20"
+    "fedora:21"
+    "fedora:22"
+    "fedora:23"
+    "fedora:24"
+    "centos:7"
+    "centos:6"
+    "centos:5"
 )
 
 for image in "${images[@]}"
