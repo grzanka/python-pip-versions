38dfa45555ee610213e7808642bdb368fe7950eb
Leszek Grzanka <grzanka@agh.edu.pl>
2016-09-28 11:07:53 +0200

    docker files

--- run.sh
+++ run.sh
@@ -9,41 +9,21 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
                 # pipeline exit successfully.
 
 # list of docker images to install python and check its version
-images=(
-    "debian:testing"
-    "debian:stable"
-    "debian:7"
-    "debian:8"
-    "ubuntu:12.04"
-    "ubuntu:14.04"
-    "ubuntu:16.04"
-    "ubuntu:16.10"
-    "fedora:20"
-    "fedora:21"
-    "fedora:22"
-    "fedora:23"
-    "fedora:24"
-    "centos:7"
-    "centos:6"
-    "centos:5"
-    "opensuse"
-    "finalduty/archlinux"
-)
-
+source images.sh
 
 # loop over all images and in each of them install python2 + pip, check python and pip versions and report them
 # then the same for python3 + pip
 :>py2.ver
 :>py3.ver
-for image in "${images[@]}"
+for IMAGE in "${IMAGES[@]}"
 do
-   docker run --rm -it -v `pwd`:/tmp/opt/:rw $image bash -c "/tmp/opt/install.sh py2; /tmp/opt/discover.sh"
-   echo $image >> py2.ver
+   docker run --rm -it -v `pwd`:/tmp/opt/:rw $IMAGE bash -c "cd /tmp/opt; ./install.sh py2; ./discover.sh"
+   echo $IMAGE >> py2.ver
    cat python.ver >> py2.ver
    cat pip.ver >> py2.ver
    echo "--------------------------------------------------" >> py2.ver
-   docker run --rm -it -v `pwd`:/tmp/opt/:rw $image bash -c "/tmp/opt/install.sh py3; /tmp/opt/discover.sh"
-   echo $image >> py3.ver
+   docker run --rm -it -v `pwd`:/tmp/opt/:rw $IMAGE bash -c "cd /tmp/opt; ./install.sh py3; ./discover.sh"
+   echo $IMAGE >> py3.ver
    cat python.ver >> py3.ver
    cat pip.ver >> py3.ver
    echo "--------------------------------------------------" >> py3.ver
